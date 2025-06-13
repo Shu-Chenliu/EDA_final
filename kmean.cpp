a3849caba9928f6013c4b9a00b2f01610e21299b
chtzhsn <chtzhsn@gmail.com>
2025-06-13 16:41:41 +0800

    test case in kmean

--- kmean.cpp
+++ kmean.cpp
@@ -237,40 +237,141 @@ int main() {
             { Pin{ Point(11, 10), 7.0, 1 } },
             { Pin{ Point(16, 16), 9.0, 2 } }
         ),
-        new FF(1, 11, 9,  "ff2"),
-        new FF(1, 9,  12, "ff3"),
+        new FF(1, 11, 9,  "ff2", 
+            { Pin{ Point(6, 6), 5.0, 1 } },
+            { Pin{ Point(14, 14), 6.0, 2 } }
+        ),
+        new FF(1, 9,  12, "ff3", 
+            { Pin{ Point(4, 4), 3.0, 1 } },
+            { Pin{ Point(13, 13), 4.0, 2 } }
+        ),
+        new FF(1, 15, 15, "ff4",
+            { Pin{ Point(10, 10), 12.0, 2 } },
+            { Pin{ Point(20, 20), 11.0, 3 } }
+        ),
+        new FF(1, 17, 16, "ff5",
+            { Pin{ Point(14, 15), 10.0, 1 }, 
+              Pin{ Point(18, 17), 9.0, 2 } },
+            { Pin{ Point(22, 22), 8.0, 3 } }
+        ),
 
-        new FF(1, 20, 20, "ff4"),
-        new FF(1, 22, 21, "ff5"),
-        new FF(1, 23, 19, "ff6"),
+        new FF(1, 20, 20, "ff4", 
+            { Pin{ Point(15, 15), 14.0, 2 } },
+            { Pin{ Point(25, 25), 13.0, 3 } }
+        ),
+        new FF(1, 21, 18, "ff5",
+            { Pin{ Point(16, 17), 12.0, 1 }, 
+              Pin{ Point(19, 20), 11.0, 2 } },
+            { Pin{ Point(23, 24), 10.0, 3 } }
+        ),
+        new FF(1, 18, 19, "ff6",
+            { Pin{ Point(13, 14), 8.0, 1 }, 
+              Pin{ Point(17, 18), 9.0, 2 },
+              Pin{ Point(20, 21), 7.0, 3 } },
+            { Pin{ Point(21, 22), 7.0, 2 },
+              Pin{ Point(24, 25), 6.0, 3 } }
+        ),          
 
-        new FF(1, 100, 100, "ff7"),
-        new FF(1, 101, 99,  "ff8"),
-        new FF(1, 99,  102, "ff9"),
-        new FF(1, 102, 101, "ff10"),
 
-        new FF(1, 250, 250, "ff11"),
-        new FF(1, 251, 249, "ff12"),
-
-        new FF(1, 60, 305, "ff13"),
-        new FF(1, 62, 307, "ff14"),
-        new FF(1, 61, 303, "ff15"),
-        new FF(1, 59, 308, "ff16"),
-
-        new FF(1, 80, 405, "ff17"),
-        new FF(1, 82, 407, "ff18"),
-        new FF(1, 81, 403, "ff19"),
-        new FF(1, 79, 408, "ff20"),
-
-        new FF(1, 300, 300, "ff21"),
-        new FF(1, 302, 301, "ff22"),
-        new FF(1, 301, 299, "ff23"),
-        new FF(1, 299, 302, "ff24"),
-
-        new FF(1, 400, 400, "ff25"),
-        new FF(1, 402, 401, "ff26"),
-        new FF(1, 401, 399, "ff27"),
-        new FF(1, 399, 402, "ff28")
+        new FF(1, 100, 100, "ff7", 
+            { Pin{ Point(95, 95), 90.0, 2 }, 
+              Pin{ Point(95, 93), 90.0, 2 }, }, // fanins
+            { Pin{ Point(105, 105), 85.0, 3 } } // fanouts
+        ),
+        new FF(1, 102, 101, "ff8", 
+            { Pin{ Point(97, 96), 88.0, 1 } }, // fanins
+            { Pin{ Point(106, 107), 84.0, 2 } } // fanouts
+        ),      
+        new FF(1, 101, 99, "ff9", 
+            { Pin{ Point(96, 94), 87.0, 1 } }, // fanins
+            { Pin{ Point(104, 103), 83.0, 2 } } // fanouts
+        ),
+        new FF(1, 99, 102, "ff10", 
+            { Pin{ Point(95, 93), 86.0, 1 } }, // fanins
+            { Pin{ Point(103, 108), 82.0, 2 } } // fanouts
+        ),
+        new FF(1, 250, 250, "ff11", 
+            { Pin{ Point(245, 245), 240.0, 2 } }, // fanins
+            { Pin{ Point(255, 255), 235.0, 3 }, 
+              Pin{ Point(255, 253), 235.0, 2 },
+              Pin{ Point(255, 252), 235.0, 2 } } // fanouts
+        ),
+        new FF(1, 251, 249, "ff12", 
+            { Pin{ Point(246, 244), 238.0, 1 } }, // fanins
+            { Pin{ Point(256, 254), 234.0, 2 } } // fanouts
+        ),
+        new FF(1, 60, 305, "ff13", 
+            { Pin{ Point(55, 300), 50.0, 2 } }, // fanins
+            { Pin{ Point(65, 310), 45.0, 3 } } // fanouts
+        ),
+        new FF(1, 62, 307, "ff14", 
+            { Pin{ Point(57, 302), 48.0, 1 } }, // fanins
+            { Pin{ Point(67, 312), 43.0, 2 } } // fanouts
+        ),
+        new FF(1, 61, 303, "ff15", 
+            { Pin{ Point(56, 301), 49.0, 1 } }, // fanins
+            { Pin{ Point(66, 308), 44.0, 2 } } // fanouts
+        ),
+        new FF(1, 59, 308, "ff16", 
+            { Pin{ Point(54, 306), 51.0, 1 } }, // fanins
+            { Pin{ Point(64, 313), 46.0, 2 } } // fanouts
+        ),
+        new FF(1, 80, 405, "ff17", 
+            { Pin{ Point(75, 400), 70.0, 2 } }, // fanins
+            { Pin{ Point(85, 410), 65.0, 3 } } // fanouts
+        ),
+        new FF(1, 82, 407, "ff18", 
+            { Pin{ Point(77, 402), 68.0, 1 } }, // fanins
+            { Pin{ Point(87, 412), 63.0, 2 } } // fanouts
+        ),
+        new FF(1, 81, 403, "ff19", 
+            { Pin{ Point(76, 401), 69.0, 1 } }, // fanins
+            { Pin{ Point(86, 408), 64.0, 2 } } // fanouts
+        ),
+        new FF(1, 79, 408, "ff20", 
+            { Pin{ Point(74, 406), 71.0, 1 } }, // fanins
+            { Pin{ Point(84, 413), 66.0, 2 } } // fanouts
+        ),
+        new FF(1, 300, 300, "ff21", 
+            { Pin{ Point(295, 295), 290.0, 2 } }, // fanins
+            { Pin{ Point(305, 305), 285.0, 3 } } // fanouts
+        ),
+        new FF(1, 302, 301, "ff22", 
+            { Pin{ Point(297, 296), 288.0, 1 } }, // fanins
+            { Pin{ Point(306, 307), 284.0, 2 }, 
+                Pin{ Point(306, 306), 284.0, 2 },
+                Pin{ Point(306, 305), 284.0, 2 } } // fanouts
+        ),
+        new FF(1, 301, 299, "ff23", 
+            { Pin{ Point(296, 294), 287.0, 1 } }, // fanins
+            { Pin{ Point(305, 304), 283.0, 2 } } // fanouts
+        ),
+        new FF(1, 299, 302, "ff24", 
+            { Pin{ Point(295, 293), 286.0, 1 } }, // fanins
+            { Pin{ Point(304, 308), 282.0, 2 } } // fanouts
+        ),
+        new FF(1, 400, 400, "ff25", 
+            { Pin{ Point(395, 395), 390.0, 2 } }, // fanins
+            { Pin{ Point(405, 405), 385.0, 3 } } // fanouts
+        ),
+        new FF(1, 402, 401, "ff26", 
+            { Pin{ Point(397, 396), 388.0, 1 } }, // fanins
+            { Pin{ Point(406, 407), 384.0, 2 }, 
+              Pin{ Point(406, 406), 384.0, 2 },
+              Pin{ Point(406, 405), 384.0, 2 } } // fanouts
+                
+        ),
+        new FF(1, 401, 399, "ff27", 
+            { Pin{ Point(396, 394), 387.0, 1 }, 
+              Pin{ Point(398, 398), 386.0, 1 }, 
+              Pin{ Point(400, 400), 385.0, 1 } }, // fanins
+            { Pin{ Point(405, 404), 383.0, 2 } } // fanouts
+        ),
+        new FF(1, 399, 402, "ff28", 
+            { Pin{ Point(395, 393), 386.0, 1 } }, // fanins
+            { Pin{ Point(404, 408), 382.0, 2 } } // fanouts
+        )
+        
     };
     for (int i = 0; i < flip_flops.size(); ++i) {
         cout << "Flop " << i << ": original=(" << flip_flops[i]->position.x << "," << flip_flops[i]->position.y
