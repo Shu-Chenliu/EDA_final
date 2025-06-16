0246741c4350ceaf0e628e541cc29592cbe0aa7a
chtzhsn <chtzhsn@gmail.com>
2025-06-16 18:01:15 +0800

    test case by me

--- main.cpp
+++ main.cpp
@@ -51,38 +51,38 @@ int main() {
 
   // Step 2: 為每個 FF 補上 fanins 與 fanouts 的 Pin，並填入 ff 指標
   flip_flops[0]->fanins = {
-      Pin{ Point(5, 5), flip_flops[0], 10.0, 2 }
+      Pin{ Point(0, 5), flip_flops[0], 10.0, 2 }
   };
   flip_flops[0]->fanouts = {
-      Pin{ Point(15, 15), flip_flops[0], 8.0, 3 }
+      Pin{ Point(15, 5), flip_flops[0], 8.0, 3 }
   };
 
   flip_flops[1]->fanins = {
       Pin{ Point(11, 10), flip_flops[1], 7.0, 1 }
   };
   flip_flops[1]->fanouts = {
-      Pin{ Point(16, 16), flip_flops[1], 9.0, 2 }
+      Pin{ Point(16, 6), flip_flops[1], 9.0, 2 }
   };
 
   flip_flops[2]->fanins = {
-      Pin{ Point(6, 6), flip_flops[2], 5.0, 1 }
+      Pin{ Point(6, 8), flip_flops[2], 5.0, 1 }
   };
   flip_flops[2]->fanouts = {
-      Pin{ Point(14, 14), flip_flops[2], 6.0, 2 }
+      Pin{ Point(4, 14), flip_flops[2], 6.0, 2 }
   };
 
   flip_flops[3]->fanins = {
       Pin{ Point(4, 4), flip_flops[3], 3.0, 1 }
   };
   flip_flops[3]->fanouts = {
-      Pin{ Point(13, 13), flip_flops[3], 4.0, 2 }
+      Pin{ Point(12, 13), flip_flops[3], 4.0, 2 }
   };
 
   flip_flops[4]->fanins = {
-      Pin{ Point(10, 10), flip_flops[4], 12.0, 2 }
+      Pin{ Point(10, 1), flip_flops[4], 12.0, 2 }
   };
   flip_flops[4]->fanouts = {
-      Pin{ Point(20, 20), flip_flops[4], 11.0, 3 }
+      Pin{ Point(2, 2), flip_flops[4], 11.0, 3 }
   };
 
   flip_flops[5]->fanins = {
@@ -90,171 +90,171 @@ int main() {
       Pin{ Point(18, 17), flip_flops[5], 9.0, 2 }
   };
   flip_flops[5]->fanouts = {
-      Pin{ Point(22, 22), flip_flops[5], 8.0, 3 }
+      Pin{ Point(12, 12), flip_flops[5], 8.0, 3 }
   };
 
   flip_flops[6]->fanins = {
-      Pin{ Point(13, 14), flip_flops[6], 8.0, 1 },
-      Pin{ Point(17, 18), flip_flops[6], 9.0, 2 },
-      Pin{ Point(20, 21), flip_flops[6], 7.0, 3 }
+      Pin{ Point(3, 1), flip_flops[6], 8.0, 1 },
+      Pin{ Point(7, 1), flip_flops[6], 9.0, 2 },
+      Pin{ Point(2, 2), flip_flops[6], 7.0, 3 }
   };
   flip_flops[6]->fanouts = {
-      Pin{ Point(21, 22), flip_flops[6], 7.0, 2 },
-      Pin{ Point(24, 25), flip_flops[6], 6.0, 3 }
+      Pin{ Point(11, 2), flip_flops[6], 7.0, 2 },
+      Pin{ Point(4, 5), flip_flops[6], 6.0, 3 }
   };
   flip_flops[7]->fanins = {
-      Pin{ Point(19, 20), flip_flops[7], 6.0, 1 },
-      Pin{ Point(23, 24), flip_flops[7], 5.0, 2 }
+      Pin{ Point(9, 2), flip_flops[7], 6.0, 1 },
+      Pin{ Point(2, 4), flip_flops[7], 5.0, 2 }
   };
   flip_flops[7]->fanouts = {
-      Pin{ Point(26, 27), flip_flops[7], 4.0, 3 }
+      Pin{ Point(6, 7), flip_flops[7], 4.0, 3 }
   };
   flip_flops[8]->fanins = {
-      Pin{ Point(25, 26), flip_flops[8], 3.0, 1 }
+      Pin{ Point(5, 6), flip_flops[8], 3.0, 1 }
   };
   flip_flops[8]->fanouts = {
-      Pin{ Point(28, 29), flip_flops[8], 2.0, 2 }
+      Pin{ Point(8, 9), flip_flops[8], 2.0, 2 }
   };
   flip_flops[9]->fanins = {
-      Pin{ Point(27, 28), flip_flops[9], 1.0, 1 }
+      Pin{ Point(7, 8), flip_flops[9], 1.0, 1 }
   };
   flip_flops[9]->fanouts = {
-      Pin{ Point(30, 31), flip_flops[9], 0.0, 2 }
+      Pin{ Point(3, 3), flip_flops[9], 0.0, 2 }
   };
   flip_flops[10]->fanins = {
-      Pin{ Point(29, 30), flip_flops[10], 1.0, 1 }
+      Pin{ Point(9, 3), flip_flops[10], 1.0, 1 }
   };
   flip_flops[10]->fanouts = {
-      Pin{ Point(32, 33), flip_flops[10], 2.0, 2 }
+      Pin{ Point(2, 13), flip_flops[10], 2.0, 2 }
   };
   flip_flops[11]->fanins = {
-      Pin{ Point(31, 32), flip_flops[11], 3.0, 1 }
+      Pin{ Point(3, 3), flip_flops[11], 3.0, 1 }
   };
   flip_flops[11]->fanouts = {
-      Pin{ Point(34, 35), flip_flops[11], 4.0, 2 }
+      Pin{ Point(3, 5), flip_flops[11], 4.0, 2 }
   };
   flip_flops[12]->fanins = {
-      Pin{ Point(33, 34), flip_flops[12], 5.0, 1 },
-      Pin{ Point(36, 37), flip_flops[12], 6.0, 2 },
-      Pin{ Point(38, 39), flip_flops[12], 7.0, 3 }
+      Pin{ Point(13, 4), flip_flops[12], 5.0, 1 },
+      Pin{ Point(16, 7), flip_flops[12], 6.0, 2 },
+      Pin{ Point(18, 19), flip_flops[12], 7.0, 3 }
   };
   flip_flops[12]->fanouts = {
-      Pin{ Point(40, 41), flip_flops[12], 8.0, 2 },
-      Pin{ Point(42, 43), flip_flops[12], 9.0, 3 }
+      Pin{ Point(4, 4), flip_flops[12], 8.0, 2 },
+      Pin{ Point(2, 4), flip_flops[12], 9.0, 3 }
   };
   flip_flops[13]->fanins = {
-      Pin{ Point(35, 36), flip_flops[13], 10.0, 1 },
-      Pin{ Point(40, 41), flip_flops[13], 11.0, 2 }
+      Pin{ Point(5, 6), flip_flops[13], 10.0, 1 },
+      Pin{ Point(4, 4), flip_flops[13], 11.0, 2 }
   };
   flip_flops[13]->fanouts = {
-      Pin{ Point(44, 45), flip_flops[13], 12.0, 3 }
+      Pin{ Point(14, 15), flip_flops[13], 12.0, 3 }
   };
   flip_flops[14]->fanins = {
-      Pin{ Point(37, 38), flip_flops[14], 13.0, 1 },
-      Pin{ Point(42, 43), flip_flops[14], 14.0, 2 }
+      Pin{ Point(7, 8), flip_flops[14], 13.0, 1 },
+      Pin{ Point(42, 13), flip_flops[14], 14.0, 2 }
   };
   flip_flops[14]->fanouts = {
-      Pin{ Point(46, 47), flip_flops[14], 15.0, 3 }
+      Pin{ Point(16, 47), flip_flops[14], 15.0, 3 }
   };
   flip_flops[15]->fanins = {
-      Pin{ Point(39, 40), flip_flops[15], 16.0, 1 },
-      Pin{ Point(44, 45), flip_flops[15], 17.0, 2 }
+      Pin{ Point(9, 4), flip_flops[15], 16.0, 1 },
+      Pin{ Point(4, 15), flip_flops[15], 17.0, 2 }
   };
   flip_flops[15]->fanouts = {
-      Pin{ Point(48, 49), flip_flops[15], 18.0, 3 }
+      Pin{ Point(18, 49), flip_flops[15], 18.0, 3 }
   };
   flip_flops[16]->fanins = {
-      Pin{ Point(41, 42), flip_flops[16], 19.0, 1 },
-      Pin{ Point(46, 47), flip_flops[16], 20.0, 2 }
+      Pin{ Point(11, 12), flip_flops[16], 19.0, 1 },
+      Pin{ Point(16, 17), flip_flops[16], 20.0, 2 }
   };
   flip_flops[16]->fanouts = {
-      Pin{ Point(50, 51), flip_flops[16], 21.0, 3 }
+      Pin{ Point(10, 5), flip_flops[16], 21.0, 3 }
   };
   flip_flops[17]->fanins = {
-      Pin{ Point(43, 44), flip_flops[17], 22.0, 1 },
-      Pin{ Point(48, 49), flip_flops[17], 23.0, 2 }
+      Pin{ Point(3, 4), flip_flops[17], 22.0, 1 },
+      Pin{ Point(8, 9), flip_flops[17], 23.0, 2 }
   };
   flip_flops[17]->fanouts = {
-      Pin{ Point(52, 53), flip_flops[17], 24.0, 3 },
-      Pin{ Point(54, 55), flip_flops[17], 25.0, 4 },
-      Pin{ Point(56, 57), flip_flops[17], 26.0, 5 }
+      Pin{ Point(2, 3), flip_flops[17], 24.0, 3 },
+      Pin{ Point(4, 5), flip_flops[17], 25.0, 4 },
+      Pin{ Point(6, 7), flip_flops[17], 26.0, 5 }
   };
   flip_flops[18]->fanins = {
-      Pin{ Point(45, 46), flip_flops[18], 27.0, 1 },
-      Pin{ Point(50, 51), flip_flops[18], 28.0, 2 }
+      Pin{ Point(4, 6), flip_flops[18], 27.0, 1 },
+      Pin{ Point(5, 1), flip_flops[18], 28.0, 2 }
   };
   flip_flops[18]->fanouts = {
-      Pin{ Point(58, 59), flip_flops[18], 29.0, 3 }
+      Pin{ Point(18, 5), flip_flops[18], 29.0, 3 }
   };
   flip_flops[19]->fanins = {
-      Pin{ Point(47, 48), flip_flops[19], 30.0, 1 },
-      Pin{ Point(52, 53), flip_flops[19], 31.0, 2 }
+      Pin{ Point(1, 18), flip_flops[19], 30.0, 1 },
+      Pin{ Point(51, 13), flip_flops[19], 31.0, 2 }
   };
   flip_flops[19]->fanouts = {
-      Pin{ Point(60, 61), flip_flops[19], 32.0, 3 }
+      Pin{ Point(10, 11), flip_flops[19], 32.0, 3 }
   }; 
   flip_flops[20]->fanins = {
-      Pin{ Point(49, 50), flip_flops[20], 33.0, 1 },
-      Pin{ Point(54, 55), flip_flops[20], 34.0, 2 }
+      Pin{ Point(19, 10), flip_flops[20], 33.0, 1 },
+      Pin{ Point(14, 15), flip_flops[20], 34.0, 2 }
   };
   flip_flops[20]->fanouts = {
-      Pin{ Point(62, 63), flip_flops[20], 35.0, 3 }
+      Pin{ Point(12, 13), flip_flops[20], 35.0, 3 }
   };
   flip_flops[21]->fanins = {
-      Pin{ Point(51, 52), flip_flops[21], 36.0, 1 },
-      Pin{ Point(56, 57), flip_flops[21], 37.0, 2 }
+      Pin{ Point(1, 5), flip_flops[21], 36.0, 1 },
+      Pin{ Point(6, 7), flip_flops[21], 37.0, 2 }
   };
   flip_flops[21]->fanouts = {
-      Pin{ Point(64, 65), flip_flops[21], 38.0, 3 }
+      Pin{ Point(6, 5), flip_flops[21], 38.0, 3 }
   };
   flip_flops[22]->fanins = {
-      Pin{ Point(53, 54), flip_flops[22], 39.0, 1 },
-      Pin{ Point(58, 59), flip_flops[22], 40.0, 2 }
+      Pin{ Point(3, 4), flip_flops[22], 39.0, 1 },
+      Pin{ Point(5, 9), flip_flops[22], 40.0, 2 }
   };
   flip_flops[22]->fanouts = {
-      Pin{ Point(66, 67), flip_flops[22], 41.0, 3 }
+      Pin{ Point(6, 7), flip_flops[22], 41.0, 3 }
   };
   flip_flops[23]->fanins = {
       Pin{ Point(55, 56), flip_flops[23], 42.0, 1 },
       Pin{ Point(60, 61), flip_flops[23], 43.0, 2 }
   };
   flip_flops[23]->fanouts = {
-      Pin{ Point(68, 69), flip_flops[23], 44.0, 3 }
+      Pin{ Point(8, 9), flip_flops[23], 44.0, 3 }
   };
   flip_flops[24]->fanins = {
-      Pin{ Point(57, 58), flip_flops[24], 45.0, 1 },
-      Pin{ Point(62, 63), flip_flops[24], 46.0, 2 }
+      Pin{ Point(5, 15), flip_flops[24], 45.0, 1 },
+      Pin{ Point(2, 6), flip_flops[24], 46.0, 2 }
   };
   flip_flops[24]->fanouts = {
-      Pin{ Point(70, 71), flip_flops[24], 47.0, 3 }
+      Pin{ Point(17, 17), flip_flops[24], 47.0, 3 }
   };
   flip_flops[25]->fanins = {
-      Pin{ Point(59, 60), flip_flops[25], 48.0, 1 },
-      Pin{ Point(64, 65), flip_flops[25], 49.0, 2 }
+      Pin{ Point(9, 6), flip_flops[25], 48.0, 1 },
+      Pin{ Point(4, 6), flip_flops[25], 49.0, 2 }
   };
   flip_flops[25]->fanouts = {
-      Pin{ Point(72, 73), flip_flops[25], 50.0, 3 }
+      Pin{ Point(2, 3), flip_flops[25], 50.0, 3 }
   };
   flip_flops[26]->fanins = {
-      Pin{ Point(61, 62), flip_flops[26], 51.0, 1 },
-      Pin{ Point(66, 67), flip_flops[26], 52.0, 2 }
+      Pin{ Point(1, 2), flip_flops[26], 51.0, 1 },
+      Pin{ Point(6, 7), flip_flops[26], 52.0, 2 }
   };
   flip_flops[26]->fanouts = {
       Pin{ Point(74, 75), flip_flops[26], 53.0, 3 }
   };
   flip_flops[27]->fanins = {
-      Pin{ Point(63, 64), flip_flops[27], 54.0, 1 },
-      Pin{ Point(68, 69), flip_flops[27], 55.0, 2 }
+      Pin{ Point(13, 14), flip_flops[27], 54.0, 1 },
+      Pin{ Point(2, 19), flip_flops[27], 55.0, 2 }
   };
   flip_flops[27]->fanouts = {
-      Pin{ Point(76, 77), flip_flops[27], 56.0, 3 }
+      Pin{ Point(26, 7), flip_flops[27], 56.0, 3 }
   };
   flip_flops[28]->fanins = {
-      Pin{ Point(65, 66), flip_flops[28], 57.0, 1 },
-      Pin{ Point(70, 71), flip_flops[28], 58.0, 2 }
+      Pin{ Point(25, 6), flip_flops[28], 57.0, 1 },
+      Pin{ Point(20, 21), flip_flops[28], 58.0, 2 }
   };
   flip_flops[28]->fanouts = {
-      Pin{ Point(78, 79), flip_flops[28], 59.0, 3 }
+      Pin{ Point(28, 29), flip_flops[28], 59.0, 3 }
   };
 
 
