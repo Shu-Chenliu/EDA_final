062d7addc8ebe3089b56c230771f30b87c04479d
Jen <sparrowjen0613@gmail.com>
2025-06-18 16:23:22 +0800

    change color for plot

--- plot.py
+++ plot.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
+# Set color
+light_purple = '#8e0a74'
+purple = '#731f7d'
+dark_purple = '#4d1354'
+light_blue = '#609ebc'
+blue = '#5b6b9e'
+
 # Load
 f = open('testcase1/testcase1.txt')
 input = []
@@ -15,7 +22,7 @@ f.close
 ax = plt.subplot()
 
 ## Board: x, y, w, h
-board = patches.Rectangle((0, 0), input[0][0], input[0][1], edgecolor='black', facecolor='none', linewidth=0.3)
+board = patches.Rectangle((0, 0), input[0][0], input[0][1], edgecolor=dark_purple, facecolor='none', linewidth=0.3)
 ax.add_patch(board)
 
 ## Bin
@@ -36,27 +43,27 @@ rowSpan, colSpan = input[1][4], input[1][5]
 inPins = int(input[2][0])
 ip = []
 for i in range(inPins):
-  ip.append(patches.Circle((input[3+i][0], input[3+i][1]), radius=1, color='red'))
+  ip.append(patches.Circle((input[3+i][0], input[3+i][1]), radius=1, color=light_purple))
   ax.add_patch(ip[i])
 
 ### outPins
 outPins = int(input[3+inPins][0])
 op = []
 for i in range(inPins):
-  op.append(patches.Circle((input[3+inPins+1+i][0], input[3+inPins+1+i][1]), radius=1, color='blue'))
+  op.append(patches.Circle((input[3+inPins+1+i][0], input[3+inPins+1+i][1]), radius=1, color=light_blue))
   ax.add_patch(op[i])
 
 ## Cell
 cells = int(input[3+inPins+1+outPins][0])
 c = []
 for i in range(cells):
-  c.append(patches.Circle((input[3+inPins+1+outPins+1+i][0], input[3+inPins+1+outPins+1+i][1]), radius=1, color='green'))
+  c.append(patches.Circle((input[3+inPins+1+outPins+1+i][0], input[3+inPins+1+outPins+1+i][1]), radius=1, color=purple))
   ax.add_patch(c[i])
 
 print("...output -ing")
 
-ax.set_xlim(-5, input[0][0]+5)
-ax.set_ylim(-5, input[0][1]+5)
+ax.set_xlim(-0.5, input[0][0]+0.5)
+ax.set_ylim(-0.5, input[0][1]+0.5)
 ax.set_aspect('equal')
 
 plt.axis('off')
